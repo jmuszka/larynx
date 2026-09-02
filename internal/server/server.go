a2823773a3945bf7a99d448e274fe1d8fd343985
Josh Muszka <josh@jmuszka.com>
2026-09-02 11:33:45 -0400

    feat: response transport compression (#29)

--- internal/server/server.go
+++ internal/server/server.go
@@ -115,6 +115,7 @@ func New(cfg Config) *Server {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.Compress(5))
 	r.Use(middleware.Timeout(30 * time.Second))
 
 	// CORS
